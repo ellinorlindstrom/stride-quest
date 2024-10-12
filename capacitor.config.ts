e963220c4e19ecfb97a8abb497f9bb514c2cb7b2
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2024-10-12 20:00:29 +0200

    added and configured capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stridequest.app',
+  appName: 'stride-quest',
+  webDir: 'build',
+};
+
+export default config;
