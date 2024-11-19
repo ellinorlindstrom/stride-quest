6e896d9368a296167fb7d1976c92fc4ab289e89d
lindstrom.elle@gmail.com <lindstrom.elle@gmail.com>
2024-11-19 13:13:08 +0100

    add mapkit and core location, change content view to show the map with user location, add permission inquiries in info.plist and add Authenticationmanager

---
+++ StrideQuest/Services/AuthenticationManager.swift
@@ -0,0 +1,75 @@
+//
+//  AuthenticationManager.swift
+//  StrideQuest
+//
+//  Created by Ellinor Lindström on 2024-11-19.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+class AuthenticationManager: ObservableObject {
+    @Published var isAuthenticated = false
+    @Published var userID: String?
+    @Published var userName: String?
+    @Published var userEmail: String?
+    
+    func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.fullName, .email]
+    }
+    
+    func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>) {
+        switch result {
+        case .success(let auth):
+            switch auth.credential {
+            case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                let userIdentifier = appleIDCredential.user
+                self.userID = userIdentifier
+                
+                let fullName = appleIDCredential.fullName
+                let email = appleIDCredential.email
+                
+                if let givenName = fullName?.givenName,
+                   let familyName = fullName?.familyName {
+                    self.userName = "\(givenName) \(familyName)"
+                }
+                
+                if let email = email {
+                    self.userEmail = email
+                }
+                
+                UserDefaults.standard.set(userIdentifier, forKey: "userIdentifier")
+                
+                DispatchQueue.main.async {
+                    self.isAuthenticated = true
+                }
+                
+                default:
+                    break
+            }
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    func signOut() {
+        
+        UserDefaults.standard.removeObject(forKey: "userIdentifier")
+        
+        DispatchQueue.main.async {
+            self.isAuthenticated = false
+            self.userID = nil
+            self.userName = nil
+            self.userEmail = nil
+        }
+    }
+    
+    func checkAuthentication() {
+        if let userIdentifier = UserDefaults.standard.string(forKey: "userIdentifier") {
+            self.userID = userIdentifier
+            self.isAuthenticated = true
+        }
+    }
+}
+
+
---
+++ StrideQuest/Services/LocationManager.swift
@@ -0,0 +1,41 @@
+import Foundation
+import CoreLocation
+import MapKit
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus
+    
+    override init() {
+        authorizationStatus = .notDetermined
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+    }
+    
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    nonisolated func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        DispatchQueue.main.async {
+            self.location = location
+        }
+    }
+    
+    nonisolated func locationManager (_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.authorizationStatus = status
+            if status == .authorizedWhenInUse {
+                self.startUpdatingLocation()
+            }
+        }
+    }
+}
